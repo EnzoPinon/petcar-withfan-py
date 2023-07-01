778356ce2816b98b0055e44cb3061c7942b81bc7
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-01 18:45:45 +0800

    define da fan

--- fan.py
+++ fan.py
@@ -4,5 +4,11 @@ Fast = 3
 
 class Fan:
 
-    def __init__(self, speed, ):
+    def __init__(self, speed, radius, is_on, color):
+        self.__speed = speed
+        self.__is_on = is_on
+        self.__color = color
+        self.__radius = radius
+    
+    
         
