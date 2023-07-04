2a2c5f0089035aa28e7e18f31cf3a435821ef8ad
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-04 15:49:24 +0800

    finished second task!

--- test_car.py
+++ test_car.py
@@ -13,3 +13,12 @@ while not first_loop is 5:
 #show speed
 details = my_car.get_speed()
 print("The speed of my", details[1], details[2], 'is currently at: ', details[0])
+
+#repeating task but in reverse.
+
+while not first_loop is 0:
+    my_car.brake()
+    first_loop = first_loop - 1
+
+updated = my_car.get_speed()
+print("The speed of my", updated[1], updated[2], 'is currently at: ', updated[0])
