15e7ce323f129efb71053f138e6a24c40cc1841d
EnzoPinon <mrlorenzopinon@gmail.com>
2023-07-04 15:37:14 +0800

    finished car class, onto testing.

--- car.py
+++ car.py
@@ -4,3 +4,14 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
+    
+    def accelerate(self):
+        self.__speed = self.__speed + 5
+        return
+    
+    def brake(self):
+        self.__speed = self.__speed - 5
+        return
+    
+    def get_speed(self):
+        return self.__speed, self.__year_model, self.__make
